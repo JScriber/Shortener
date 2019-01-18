0fed81da6077d54f15ac3aba76b1c9165bfcd142
Yannoël <yannoel.martin@hotmail.fr>
2019-01-18 10:27:53 +0100

    maj

--- app/app.spec.js
+++ app/app.spec.js
@@ -1,16 +1,15 @@
 // importation des modules
+import app from './app'
 import access from './services/business-logic/link/access/access.spec'
 import create from './services/business-logic/link/create/create.spec'
 import suppression from './services/business-logic/link/delete/delete.spec'
 import list from './services/business-logic/link/list/list.spec'
 
-// remise à 0 de la db
-function reset() {
-}
+// réinitialisation des mocks avant chaque test
+// beforeEach(jest.restoreAllMocks)
 
-// réinitialisation des mocjs avant chaque test
-beforeEach(jest.restoreAllMocks)
-
-// reset de la base de donnée à la fin de chaque test
-afterAll(reset)
-
+// describe('app.js', () => {
+//     test('', () => {
+//         expect(access(4)).toBe('night')
+//     })
+// })
