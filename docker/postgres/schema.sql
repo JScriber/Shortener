323a9d835fa7fbc1ca46d54581b3a6ea891def8d
Anthony <anthonygst26@gmail.com>
2019-01-18 09:51:10 +0100

    New database structure

--- docker/postgres/schema.sql
+++ docker/postgres/schema.sql
@@ -1,8 +1,36 @@
-CREATE TABLE public.link(
-	hash varchar(255) NOT NULL,
-	url varchar(255),
-	visits smallint,
-	CONSTRAINT hash PRIMARY KEY (hash)
-);
 
+DROP TABLE IF EXISTS public.link CASCADE;
+CREATE TABLE public.link(
+	id serial NOT NULL,
+	hash varchar(255),
+	url varchar(255),
+	created_at date,
+	id_user integer,
+	CONSTRAINT link_id PRIMARY KEY (id)
+
+);
+-- ddl-end --
 ALTER TABLE public.link OWNER TO postgres;
+-- ddl-end --
+
+DROP TABLE IF EXISTS public."user" CASCADE;
+CREATE TABLE public."user"(
+	id serial NOT NULL,
+	name varchar(255),
+	password varchar(255),
+	salt varchar(255),
+	token varchar(255),
+	CONSTRAINT user_id PRIMARY KEY (id)
+
+);
+-- ddl-end --
+ALTER TABLE public."user" OWNER TO postgres;
+-- ddl-end --
+
+-- object: user_fk | type: CONSTRAINT --
+-- ALTER TABLE public.link DROP CONSTRAINT IF EXISTS user_fk CASCADE;
+ALTER TABLE public.link ADD CONSTRAINT user_fk FOREIGN KEY (id_user)
+REFERENCES public."user" (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+-- ddl-end --
+
