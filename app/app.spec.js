c62fc91009af5ffef226bfab6a7348c901bd755c
Yannoël <yannoel.martin@hotmail.fr>
2019-01-18 10:02:08 +0100

    import des premières instances a testée

--- app/app.spec.js
+++ app/app.spec.js
@@ -1,4 +1,8 @@
 // importation des modules
+import access from './services/business-logic/link/access/access.spec'
+import create from './services/business-logic/link/create/create.spec'
+import suppression from './services/business-logic/link/delete/delete.spec'
+import list from './services/business-logic/link/list/list.spec'
 
 // remise à 0 de la db
 
