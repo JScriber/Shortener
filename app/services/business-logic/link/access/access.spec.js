7549ded35a2f29fb2fac9fd9e950ec00ec1c460d
Yannoël <yannoel.martin@hotmail.fr>
2019-01-18 16:04:10 +0100

    avancées

--- app/services/business-logic/link/access/access.spec.js
+++
@@ -1,9 +0,0 @@
-import { accessLink } from "./access";
-
-describe('Access service. ', () => {
-  it('should return nothing.', async () => {
-    const response = await accessLink();
-
-    expect(response).not.toBeDefined();
-  });
-});
